c7a880b835a1fc0c82afc3a00e32a0423fb95260
Ana Belen Jover Ballester <anab.jover@gmail.com>
2022-05-01 23:12:13 +0200

    Wdithout Bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,97 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack = () => {
+    return this.strength;
+  }
+
+  receiveDamage = (damage) => {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super(health, strength)
+    this.name = name;
+  }
+
+  receiveDamage = (damage) => {
+    this.health -= damage;
+    if (this.health > 0) {
+    return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+
+  battleCry = () => {
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor (health, strength) {
+  super(health, strength)
+  }
+
+  attack = () => {
+    return this.strength;
+  }
+
+  receiveDamage = (damage) => {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor () {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    
+  }
+
+  addViking = (viking) => {
+    this.vikingArmy.push(viking);
+  }
+
+  addSaxon = (saxon) => {
+    this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack = () => {
+    const randomVikingNumber = Math.floor(Math.random() * this.vikingArmy.length - 1);
+    const randomViking = this.vikingArmy[randomVikingNumber];
+    const randomSaxonNumber = Math.floor(Math.random() * this.saxonArmy.length - 1);
+    const randomSaxon = this.saxonArmy[randomSaxonNumber];
+
+    const result = randomSaxon.receiveDamage(randomViking.attack());
+    return result;
+  }
+
+  saxonAttack = () => {
+
+  }
+
+  showStatus = () => {
+
+  }
+
+}
+
+
 
 
 
